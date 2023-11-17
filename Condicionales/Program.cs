1b7fea851c06c8adf29e8ba8d8c054bef10f4e86
juan cruz carisimo <juancruzc278@gmail.com>
2023-11-16 22:22:32 -0300

    Agregar archivos de proyecto.

---
+++ Condicionales/Program.cs
@@ -0,0 +1,20 @@
+﻿// Ejemplo de condicional if
+
+Console.WriteLine("Ingrese su edad: "); //Le pedimos al usario que ingrese su edad
+int edad = int.Parse(Console.ReadLine());//Obtenemos la edad por teclado
+
+Console.WriteLine("La edad ingresada es: " + edad);
+
+Console.WriteLine("Ingrese su equipo de futbol: ");
+string equipo = Console.ReadLine();
+
+if (edad < 18 && equipo == "river") //Evaluamos la edad
+{
+    //esta instruccion solo se ejecuta si la edad ingresada es menor a 18
+    Console.WriteLine("Usted es menor de edad.");
+    Console.WriteLine("Tiene prohibida la entrada.");
+}
+else
+{
+    Console.WriteLine("Usted es mayor de edad.");
+}
